b74cb0a97eadf865879cdd6f69b6054a1b38caa9
Neezar <rforlyffe@gmaicl.com>
2022-08-04 01:54:54 +0800

    fix path

--- server/packages/kanban-service/src/main.ts
+++ server/packages/kanban-service/src/main.ts
@@ -4,10 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { SocketIoAdapter } from './adapters/socket-io.adapter';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   let corsOrigins = ['http://localhost:3030', 'http://localhost:3002'];
+  app.use(cookieParser());
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.REDIS,
